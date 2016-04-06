8f70358a08e1c2d4f7df630d9e9a65f42aa3adb0
k2iga <k2iga7@gmail.com>
2016-04-06 11:13:34 +0200

    with comment

--- Matrix.cpp
+++ Matrix.cpp
@@ -14,84 +14,79 @@ Matrix::Matrix()
 
 vector<vector<int>> Matrix::addMatrices()
 {
-
  vector<vector<int>>resultant_matrix;
 
+ //Show how to type
  printMatrixInputMethod();
 
+ //Take values for first matrix
  vector<vector<int>>matrix_1 = putin();
 
+ //Take values for second matrix
  cout << "\n Please enter the values of latter matrix:" << endl << endl;
-
  vector<vector<int>>matrix_2= putin();
 
-
- int r_max = matrix_1.size();      //Number of rows
- int c_max = matrix_1[0].size();   //Number of columns
-
+ int r_max = matrix_1.size();    //Number of rows (assuming is the same for both matrices) 
+ int c_max = matrix_1[0].size();  //Number of columns
 
  //Matrix addition
-
  for (int i = 0; i < r_max; i++)
  {
 	 vector<int> v;
 	 resultant_matrix.push_back(v);
-	 //resultant_matrix.push_back(*(new vector<int>));
+	 // resultant_matrix.push_back(*(new vector<int>));
 	 for (int j = 0; j < c_max; j++)
 	 {
 		 resultant_matrix[i].push_back( matrix_1[i][j] + matrix_2[i][j]);
 	 }
  }
-
+ 
+ //Printout of resultant matrix
  cout << "\n  Values of the resultant matrix: " << endl << endl;
-
  printoutMatrix(resultant_matrix);
 
  return resultant_matrix;
 }
 
-//Taking values of matrix from user  PUTIN 
+//Taking values of matrix from user   
 
 vector<vector<int>> Matrix::putin()
-
 {
 	vector<vector<int>>matrix_one;
 	vector<string>first_split;
-	string::size_type position;
+	string::size_type search_for_q;
 
 	string user_input;
 
-	int size_of_row = 1;//moze na dol
+	int size_of_row = 1;// default lenght of the shortest row in matrix, assumes at least 1 value
 
 	do
 	{
-		cin >> user_input;
-		position = user_input.find("q"); // search for "q"
+		cin >> user_input; // take string from user
+		search_for_q = user_input.find("q"); // search for "q" in this string ***
 
+		//Split string and get vector with values: string"1,2,3,q" => vector<string> ={1,2,3,q}
 			first_split = split(user_input);
 
-        //Resize vector if there is "q" inside, cause q can't be transform into integer
+        //Get rid of "q"(if there is any): vector ={1,2,3,q} => vector ={1,2,3}
 			for (int i = 0; i < first_split.size(); i++)
 			{
 				if (first_split[i]=="q")
 				{
 					first_split.resize(first_split.size()-1);
 				}
-
 			}
-
-          
-		//Fill empty spaces with zeros
+  
+		//Fill empty spaces with zeros: {2,,3} => {2,0,3}
 			for (int i = 0; i < first_split.size(); i++)
 			{
 				if (first_split[i].empty())
 				{
 					first_split[i] = "0";
 				}
-				
 			}
 			
-			//Convertion to integers and push to row vector
+			//Convertion strings to integers-"stoi" and push numbers to row vector, 
 			vector<int>row_vector;
 
 			for (int i = 0; i < first_split.size(); i++)
@@ -100,41 +95,39 @@ vector<vector<int>> Matrix::putin()
 				//cout << "\t" << row_vector[i];   //not
 			}
 
+			//*Check if created vector is longest than previous one (default size is set to 1) 
 			if (size_of_row < row_vector.size())
 			{
-				size_of_row = row_vector.size();
-			}
+				size_of_row = row_vector.size(); //* if it is longest - we set a new value for size_of_row    
+			}                                    //* so "size_of_row" is maximal length of vector
 
 			matrix_one.push_back(row_vector);
 
-	} while (position == string::npos);
+	} while (search_for_q == string::npos); //*** loop will end when user types "q", 
+                                            //*** "find" function return position of searched substring, "Aq47"--> return 2
+											//*** if it doesn't find it returns "npos" -largest int (4294967295)  
 
-
-	//Resize to have the same length of rows
-
-	for (int i = 0; i < matrix_one.size(); i++)
+//*Resize all vectors in matrix to have the same size - which is specifed by "size_of_row"
+ for (int i = 0; i < matrix_one.size(); i++)
 	{
 		if (matrix_one[i].size() < size_of_row)
 
 		{
-			matrix_one[i].resize(size_of_row, 0);
+			matrix_one[i].resize(size_of_row, 0); // resize vectors and fill extra place with zeros 
 		}
 	}
 
-
-
-			std::cout << "\n Congrats, Here is your matrix! " << endl << endl;
-
-			//Printout of user input as a matrix
-
+ //Printout of user input as a matrix
+            cout << "\n Congrats, Here is your matrix! " << endl << endl;
 			printoutMatrix(matrix_one);
 
 	return  matrix_one;
 
 }
 
-
 //*************************************************************************************************************
+
+//Split string with delimeter
 
 vector<string> Matrix::split(const string & s, char delim)
 {
@@ -150,7 +143,6 @@ vector<string> Matrix::split(const string & s, char delim)
 	return elems; //return vector 
 
 }
-
 
 //Printout of matrix
 
@@ -172,7 +164,6 @@ void Matrix::printoutMatrix(vector<vector<int>> matrix)
 	}
 }
 
-
 //Information for the user how matrix should be entered
 
 void Matrix::printMatrixInputMethod()
@@ -192,8 +183,6 @@ void Matrix::printMatrixInputMethod()
 
 
 }
-
-
 
 //void Matrix::printVector(vector<int>)
 
